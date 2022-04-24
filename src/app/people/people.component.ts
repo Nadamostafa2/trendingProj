f5212b226325cafa5ea2e340e8ad578dac520a72
Nadamostafa2 <48297055+Nadamostafa2@users.noreply.github.com>
2022-04-24 17:53:18 +0200

    add trending people

--- src/app/people/people.component.ts
+++ src/app/people/people.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TrendingService } from '../trending.service';
 
 @Component({
   selector: 'app-people',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./people.component.scss']
 })
 export class PeopleComponent implements OnInit {
+  trendingPersons:any[]=[];
 
-  constructor() { }
+  constructor(private trendingService:TrendingService) { }
 
   ngOnInit(): void {
+     this.getTrendingPersons();
+  }
+  getTrendingPersons() {
+    this.trendingService.getTrending("person").subscribe((response: any) => {
+      console.log(response);
+      this.trendingPersons = response.results.filter((data:any)=>{
+        return data.profile_path != null;
+      });
+      
+    })
+    
   }
 
 }
